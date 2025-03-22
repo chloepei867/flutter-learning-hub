8dae355f59d23065a42f0a4314b3307dd0455a7e
chloepei867 <pei.zhiy@northeastern.edu>
2025-03-21 18:25:28 -0700

    update project clima

--- projects/08_clima-flutter/lib/services/weather.dart
+++ projects/08_clima-flutter/lib/services/weather.dart
@@ -1,7 +1,7 @@
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 
-const apiKey = "33b650ba5a9ba66cb905b832bd0a619f";
+const apiKey = "YOUR-API-KEY";
 const openWeatherMapURL = "https://api.openweathermap.org/data/2.5/weather";
 
 class WeatherModel {
